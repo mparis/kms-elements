89e1e83d776f8d3c625839d24a6d06bc91bf640c
Miguel París Díaz <mparisdiaz@gmail.com>
2015-04-23 18:25:43 +0200

    (TODO)constructors: fix test config
    TODO: why not "modules.kurento.SdpEndpoint.audioCodecs"
    Change-Id: I5889c2000f2bf45c7eaa1d16d453a2b6d92eb55c

--- tests/server/constructors.cpp
+++ tests/server/constructors.cpp
@@ -189,8 +189,10 @@ main (int argc, char **argv)
                     Json::Value() );
 
   config.add ("configPath", "../../../tests" );
-  config.add ("modules.kurento.SdpEndpoint.sdpPattern", "sdp_pattern.txt");
-  config.add ("modules.kurento.SdpEndpoint.configPath", "../../../tests");
+  config.add ("modules.kurento.SdpEndpoint.numAudioMedias", 0);
+  config.add ("modules.kurento.SdpEndpoint.numVideoMedias", 0);
+  config.add ("modules.SdpEndpoint.audioCodecs", "[]");
+  config.add ("modules.SdpEndpoint.videoCodecs", "[]");
 
   testHttpGetEndPoint (moduleManager, mediaPipeline);
   testHttpPostEndPoint (moduleManager, mediaPipeline);
